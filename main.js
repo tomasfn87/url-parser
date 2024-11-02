0178dc968ccabfffa986684c7a6917c554d877d9
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2024-11-02 17:05:20 -0300

    General refactoring and invalid configuration file error handling.

--- main.js
+++ main.js
@@ -9,78 +9,86 @@ const main = () => {
     const object = (process.argv[3] || '').toLowerCase();
     const r = parseUrl(input);
     if (object === "native") {
-        console.log(r);
+        console.log(r.parts);
+        return;}
+    if (!r) {
+        console.log("Invalid URL.");
         return;}
     const {title, subtitle, content} = loadConfigFile("config.json");
-    if (r) {
-        color.log(title, "Domain");
+    color.log(title, "Domain");
+    process.stdout.write(":\n- ");
+    color.log(content, `${r.parts.domain}\n\n`);
+    if (r.parts.path) {
+        color.log(title, "Path");
         process.stdout.write(":\n- ");
-        color.log(content, `${r.domain}\n\n`);
-        if (r.path) {
-            color.log(title, "Path");
-            process.stdout.write(":\n- ");
-            color.log(content, `${r.path}\n\n`);}
-        if (r.parameters) {
-            color.log(title, "Parameters");
-            process.stdout.write(":\n");}
-        if (Object.keys(r.parameters_obj).length) {
-            let maxLength = 0;
-            Object.entries(r.parameters_obj).forEach(e => {
-                if (e[0].length > maxLength) maxLength = e[0].length;});
-            Object.entries(r.parameters_obj).forEach(e => {
-                process.stdout.write("- ");
+        color.log(content, `${r.parts.path}\n\n`);}
+    let maxLength = 0;
+    if (r.parts.parameters.str) {
+        color.log(title, "Parameters");
+        process.stdout.write(":\n");}
+    if (Object.keys(r.parts.parameters.obj).length) {
+        Object.entries(r.parts.parameters.obj).forEach(e => {
+            if (e[0].length > maxLength) maxLength = e[0].length;});
+        Object.entries(r.parts.parameters.obj).forEach(e => {
+            process.stdout.write("- ");
+            color.log(subtitle, `${e[0].padStart(maxLength)}`)
+            process.stdout.write(":");
+            color.log(content, ` ${e[1]}\n`);});
+        console.log();}
+    if (r.parts.fragment.str) {
+        color.log(title, "Fragment");
+        process.stdout.write(":\n");}
+    if (Object.keys(r.parts.fragment.obj).length) {
+        Object.entries(r.parts.fragment.obj).forEach(e => {
+            if (e[0].length > maxLength) maxLength = e[0].length;});
+        Object.entries(r.parts.fragment.obj).forEach(e => {
+            process.stdout.write("- ");
+            if (e[1]) {
                 color.log(subtitle, `${e[0].padStart(maxLength)}`)
                 process.stdout.write(":");
-                color.log(content, ` ${e[1]}\n`);});
-            console.log();}
-        if (r.fragment) {
-            color.log(title, "Fragment");
-            process.stdout.write(":\n");}
-        if (Object.keys(r.fragment_obj).length) {
-            let maxLength = 0;
-            Object.entries(r.fragment_obj).forEach(e => {
-                if (e[0].length > maxLength) maxLength = e[0].length;});
-            Object.entries(r.fragment_obj).forEach(e => {
-                process.stdout.write("- ");
-                if (e[1]) {
-                    color.log(subtitle, `${e[0].padStart(maxLength)}`)
-                    process.stdout.write(":");
-                    color.log(content, ` ${e[1]}`);}
-                else {
-                    color.log(content, `${e[0].padStart(maxLength)}`)}
-                console.log();});
-            console.log();}
-        const fullUrl = (r.domain + (r.path || '') + (r.parameters || '')
-            + (r.fragment || ''));
-        if (input == fullUrl) {
-            color.log(title, "Full URL");
-            process.stdout.write(":\n- ");
-            color.log(content, `${fullUrl}\n`);}}
-    else
-        console.log("Invalid URL.");}
+                color.log(content, ` ${e[1]}`);}
+            else
+                color.log(content, `${e[0].padStart(maxLength)}`);
+            console.log();});
+        console.log();}
+    if (input == r.fullUrl()) {
+        color.log(title, "Full URL");
+        process.stdout.write(":\n- ");
+        color.log(content, `${r.fullUrl()}\n`);}}
 
 const loadConfigFile = (file) => {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
     const configFile = path.join(__dirname, file);
     const defaultConfig = {
-        title: "brightCyan", subtitle: "cyan", content: "default"};
+        title: "cyan", subtitle: "brightCyan", content: "default"};
     if (!fs.existsSync(configFile))
         fs.writeFileSync(configFile, JSON.stringify(defaultConfig, null, 2));
-    const config = JSON.parse(fs.readFileSync(configFile, "utf-8"));
+    let config = defaultConfig;
+    try {
+        config = JSON.parse(fs.readFileSync(configFile, "utf-8"));
+        if (!Object.keys(config).includes("title") ||
+            !Object.keys(config).includes("subtitle") ||
+            !Object.keys(config).includes("content")) {
+            config = defaultConfig;
+            fs.writeFileSync(configFile, JSON.stringify(config, null, 2));}}
+    catch {
+        config = defaultConfig;
+        fs.writeFileSync(configFile, JSON.stringify(config, null, 2));}
     let error = false;
     Object.entries(config).forEach(e => {
         if (!color.validate(e[1])) {
-            color.log("dim", "The color ");
+            error = true;
+            color.log("brightRed", "Error");
+            color.log("dim", ": the color ");
             color.log("brightRed", e[1]);
             color.log("dim", " is not a valid option for ");
-            color.log("default", e[0])
+            color.log("default", e[0]);
             color.log("dim", ". The color ");
             color.log(defaultConfig[e[0]], defaultConfig[e[0]]);
             color.log("dim", " will be used instead.\n");
-            config[e[0]] = defaultConfig[e[0]];
-            fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
-            error = true;}})
+            config[e[0]] = defaultConfig[e[0]];}})
     if (error) {
+        fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
         process.stdout.write("\nSuggested colors: ");
         const skip = 6;
         for (let i=0; i<color.list().slice(skip).length - 2; i++) {
