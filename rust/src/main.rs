930af42029e36048b1722d6eaa0b161f2e389d59
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2025-06-21 14:13:44 -0300

    Added main parse operation: split URL into origin, path, parameters and fragment.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,15 @@
 mod url_parser;
 
+use std::env;
+
 fn main() {
-    url_parser::Url::parse_url();
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        println!("* Enter a URL to be parsed.");
+        return;
+    }
+    let user_input = &args[1];
+    let url = url_parser::Url::parse_url(user_input);
     println!();
-    url_parser::Url::full_url();
+    println!("{}", url.expect("Error").full_url());
 }
--- rust/src/url_parser.rs
+++ rust/src/url_parser.rs
@@ -1,3 +1,6 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
 struct KeyOptionalValue {
     key: String,
     optional_value: Option<String>,
@@ -17,14 +20,72 @@ pub struct Url {
 }
 
 impl Url {
-    pub fn parse_url() {
-        println!("https://site.com");
-        println!("/");
-        println!("?q=test");
-        println!("#about");
+    pub fn parse_url(url: &str) -> Result<Url, String> {
+        lazy_static! {
+            static ref URL_PARTS: Regex = Regex::new(
+                r"^((?:\w+:\/\/)?[^\/.:]+(?:\.[^\/.?#]+)+)((?:\/?(?:[^\/?#]+)?)*)?(\?(?:[^?#]+?)?)?(#(?:[^#]+?)?)?$"
+            ).unwrap();
+        }
+        let captures = URL_PARTS.captures(url)
+            .ok_or_else(|| {
+                format!("Failed to parse URL '{}'. It does not match the expected format.", url)
+            })?;
+
+        let parsed_origin = if let Some(m) = captures.get(1) {
+            m.as_str().to_string()
+        } else {
+            return Err(format!("Could not extract origin from URL '{}'.", url));
+        };
+
+        let mut parsed_path = captures.get(2)
+                                      .map_or("", |m| m.as_str())
+                                      .to_string();
+        if parsed_path.is_empty() {
+            parsed_path = "/".to_string();
+        }
+
+        let parsed_parameters_data = captures.get(3)
+                                             .map_or("", |m| m.as_str())
+                                             .to_string();
+
+        let parsed_fragment_data = captures.get(4)
+                                           .map_or("", |m| m.as_str())
+                                           .to_string();
+
+        println!("* Origin:     {}", parsed_origin);
+        println!("* Path:       {}", parsed_path);
+        println!("* Parameters: {}", parsed_parameters_data);
+        println!("* Fragment:   {}", parsed_fragment_data);
+        
+        Ok(Url {
+            origin: parsed_origin,
+            path: parsed_path,
+            parameters: KeyOptionalValueData {
+                data: parsed_parameters_data,
+                list: Vec::new(),
+                obj: Vec::new(),
+            },
+            fragment: KeyOptionalValueData {
+                data: parsed_fragment_data,
+                list: Vec::new(),
+                obj: Vec::new(),
+            },
+        })
     }
 
-    pub fn full_url() {
-        println!("https://site.com/?q=test#about");
+    pub fn full_url(&self) -> String {
+        let mut full_url_string = String::new();
+        full_url_string.push_str(&self.origin);
+        full_url_string.push_str(&self.path);
+
+        if !self.parameters.data.is_empty() {
+            full_url_string.push_str(&self.parameters.data);
+        }
+
+        if !self.fragment.data.is_empty() {
+            full_url_string.push_str(&self.fragment.data);
+        }
+
+        full_url_string
     }
 }
