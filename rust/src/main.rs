001b42d765f9de1c8ebd1e73a28716e31ce929ed
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2025-07-13 09:00:09 -0300

    Improved error messages for empty and invalid URL inputs.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,16 +1,19 @@
 mod url_parser;
 
+use ansi_term::Colour::Red;
 use std::env;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() < 2 {
-        println!("* Enter a URL to be parsed.");
+        println!("{}: {}", Red.paint("ERROR"), "empty URL. Insert a URL and try again.");
         return;
     }
     let user_input = &args[1];
-    let parsed_url = url_parser::Url::parse_url(user_input)
-        .expect("Error parsing URL");
-    parsed_url.print_url(args.len() > 2 && &args[2] == "--decode");
-    parsed_url.print_full_url();
+    if let Ok(parsed_url) = url_parser::Url::parse_url(user_input) {
+        parsed_url.print_url(args.len() > 2 && &args[2] == "--decode");
+        parsed_url.print_full_url();
+    } else {
+        println!("{}: {}", Red.paint("ERROR"), "invalid URL. Insert a valid URL and try again.");
+    }
 }
