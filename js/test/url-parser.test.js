adc36969f40f78a01ed2a77f4b383a0ab1aad3dc
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2025-10-12 02:34:58 -0300

    Added test cases for empty values (key and delimiter are present but the value is empty).

--- js/test/url-parser.test.js
+++ js/test/url-parser.test.js
@@ -2,35 +2,50 @@ import { ClassMethodTest, ClassMethodTester }
     from '../util/test-class-method.js';
 import Url from '../src/url-parser.js';
 
+const testUrls = [
+    "https://tagassistant.google.com/?hl=en&utm_source=gtm#/?source=TAG_MANAGER&id=GTM-MXNVD4PJ&gtm_auth=H0X5tN2DEEpfJ0eha8q7Ig&gtm_preview=env-5&cb=5607611040920180",
+    "https://query.frag/#?",
+    "https://empty.parts/?#",
+    "https://test.com/?q=test&id=21afea8219bc#privacy",
+    "https://test.com?q=test&id=21afea8219bc#privacy" ,
+    "https://empty.value/?q=test&dm=",
+    "-"
+];
+
 const getOriginTest = new ClassMethodTest(Url, "getOrigin", [], "Url.getOrigin");
 getOriginTest.addTestCases([
     {
-        input: { url: "https://tagassistant.google.com/?hl=en&utm_source=gtm#/?source=TAG_MANAGER&id=GTM-MXNVD4PJ&gtm_auth=H0X5tN2DEEpfJ0eha8q7Ig&gtm_preview=env-5&cb=5607611040920180" },
+        input: { url: testUrls[0] },
         keys: [ "url" ],
         expectedOutput: "https://tagassistant.google.com"
     },
     {
-        input: { url: "https://query.frag/#?" },
+        input: { url: testUrls[1] },
         keys: [ "url" ],
         expectedOutput: "https://query.frag"
     },
     {
-        input: { url: "https://empty.parts/?#" },
+        input: { url: testUrls[2] },
         keys: [ "url" ],
         expectedOutput: "https://empty.parts"
     },
     {
-        input: { url: "https://test.com/?q=test&id=21afea8219bc#privacy" },
+        input: { url: testUrls[3] },
         keys: [ "url" ],
         expectedOutput: "https://test.com"
     },
     {
-        input: { url: "https://test.com?q=test&id=21afea8219bc#privacy" },
+        input: { url: testUrls[4] },
         keys: [ "url" ],
         expectedOutput: "https://test.com"
     },
     {
-        input: { url: "-" },
+        input: { url: testUrls[5] },
+        keys: [ "url" ],
+        expectedOutput: "https://empty.value"
+    },
+    {
+        input: { url: testUrls[6] },
         keys: [ "url" ],
         expectedOutput: ""
     }
@@ -39,32 +54,37 @@ getOriginTest.addTestCases([
 const getPathTest = new ClassMethodTest(Url, "getPath", [], "Url.getPath");
 getPathTest.addTestCases([
     {
-        input: { url: "https://tagassistant.google.com/?hl=en&utm_source=gtm#/?source=TAG_MANAGER&id=GTM-MXNVD4PJ&gtm_auth=H0X5tN2DEEpfJ0eha8q7Ig&gtm_preview=env-5&cb=5607611040920180" },
+        input: { url: testUrls[0] },
         keys: [ "url" ],
         expectedOutput: "/"
     },
     {
-        input: { url: "https://query.frag/#?" },
+        input: { url: testUrls[1] },
         keys: [ "url" ],
         expectedOutput: "/"
     },
     {
-        input: { url: "https://empty.parts/?#" },
+        input: { url: testUrls[2] },
         keys: [ "url" ],
         expectedOutput: "/"
     },
     {
-        input: { url: "https://test.com/?q=test&id=21afea8219bc#privacy" },
+        input: { url: testUrls[3] },
         keys: [ "url" ],
         expectedOutput: "/"
     },
     {
-        input: { url: "https://test.com?q=test&id=21afea8219bc#privacy" },
+        input: { url: testUrls[4] },
         keys: [ "url" ],
         expectedOutput: "/"
     },
     {
-        input: { url: "-" },
+        input: { url: testUrls[5] },
+        keys: [ "url" ],
+        expectedOutput: "/"
+    },
+    {
+        input: { url: testUrls[6] },
         keys: [ "url" ],
         expectedOutput: ""
     }
@@ -73,32 +93,37 @@ getPathTest.addTestCases([
 const getParametersTest = new ClassMethodTest(Url, "getParameters", [], "Url.getParameters");
 getParametersTest.addTestCases([
     {
-        input: { url: "https://tagassistant.google.com/?hl=en&utm_source=gtm#/?source=TAG_MANAGER&id=GTM-MXNVD4PJ&gtm_auth=H0X5tN2DEEpfJ0eha8q7Ig&gtm_preview=env-5&cb=5607611040920180" },
+        input: { url: testUrls[0] },
         keys: [ "url" ],
         expectedOutput: "?hl=en&utm_source=gtm"
     },
     {
-        input: { url: "https://query.frag/#?" },
+        input: { url: testUrls[1] },
         keys: [ "url" ],
         expectedOutput: ""
     },
     {
-        input: { url: "https://empty.parts/?#" },
+        input: { url: testUrls[2] },
         keys: [ "url" ],
         expectedOutput: "?"
     },
     {
-        input: { url: "https://test.com/?q=test&id=21afea8219bc#privacy" },
+        input: { url: testUrls[3] },
         keys: [ "url" ],
         expectedOutput: "?q=test&id=21afea8219bc"
     },
     {
-        input: { url: "https://test.com?q=test&id=21afea8219bc#privacy" },
+        input: { url: testUrls[4] },
         keys: [ "url" ],
         expectedOutput: "?q=test&id=21afea8219bc"
     },
     {
-        input: { url: "-" },
+        input: { url: testUrls[5] },
+        keys: [ "url" ],
+        expectedOutput: "?q=test&dm="
+    },
+    {
+        input: { url: testUrls[6] },
         keys: [ "url" ],
         expectedOutput: ""
     }
@@ -107,32 +132,37 @@ getParametersTest.addTestCases([
 const getFragmentTest = new ClassMethodTest(Url, "getFragment", [], "Url.getFragment");
 getFragmentTest.addTestCases([
     {
-        input: { url: "https://tagassistant.google.com/?hl=en&utm_source=gtm#/?source=TAG_MANAGER&id=GTM-MXNVD4PJ&gtm_auth=H0X5tN2DEEpfJ0eha8q7Ig&gtm_preview=env-5&cb=5607611040920180" },
+        input: { url: testUrls[0] },
         keys: [ "url" ],
         expectedOutput: "#/?source=TAG_MANAGER&id=GTM-MXNVD4PJ&gtm_auth=H0X5tN2DEEpfJ0eha8q7Ig&gtm_preview=env-5&cb=5607611040920180"
     },
     {
-        input: { url: "https://query.frag/#?" },
+        input: { url: testUrls[1] },
         keys: [ "url" ],
         expectedOutput: "#?"
     },
     {
-        input: { url: "https://empty.parts/?#" },
+        input: { url: testUrls[2] },
         keys: [ "url" ],
         expectedOutput: "#"
     },
     {
-        input: { url: "https://test.com/?q=test&id=21afea8219bc#privacy" },
+        input: { url: testUrls[3] },
         keys: [ "url" ],
         expectedOutput: "#privacy"
     },
     {
-        input: { url: "https://test.com?q=test&id=21afea8219bc#privacy" },
+        input: { url: testUrls[4] },
         keys: [ "url" ],
         expectedOutput: "#privacy"
     },
     {
-        input: { url: "-" },
+        input: { url: testUrls[5] },
+        keys: [ "url" ],
+        expectedOutput: ""
+    },
+    {
+        input: { url: testUrls[6] },
         keys: [ "url" ],
         expectedOutput: ""
     }
@@ -141,32 +171,37 @@ getFragmentTest.addTestCases([
 const getFullUrlTest = new ClassMethodTest(Url, "getFullUrl", [], "Url.getFullUrl");
 getFullUrlTest.addTestCases([
     {
-        input: { url: "https://tagassistant.google.com/?hl=en&utm_source=gtm#/?source=TAG_MANAGER&id=GTM-MXNVD4PJ&gtm_auth=H0X5tN2DEEpfJ0eha8q7Ig&gtm_preview=env-5&cb=5607611040920180" },
+        input: { url: testUrls[0] },
         keys: [ "url" ],
         expectedOutput: "https://tagassistant.google.com/?hl=en&utm_source=gtm#/?source=TAG_MANAGER&id=GTM-MXNVD4PJ&gtm_auth=H0X5tN2DEEpfJ0eha8q7Ig&gtm_preview=env-5&cb=5607611040920180"
     },
     {
-        input: { url: "https://query.frag/#?" },
+        input: { url: testUrls[1] },
         keys: [ "url" ],
         expectedOutput: "https://query.frag/#?"
     },
     {
-        input: { url: "https://empty.parts/?#" },
+        input: { url: testUrls[2] },
         keys: [ "url" ],
         expectedOutput: "https://empty.parts/?#"
     },
     {
-        input: { url: "https://test.com/?q=test&id=21afea8219bc#privacy" },
+        input: { url: testUrls[3] },
         keys: [ "url" ],
         expectedOutput: "https://test.com/?q=test&id=21afea8219bc#privacy"
     },
     {
-        input: { url: "https://test.com?q=test&id=21afea8219bc#privacy" },
+        input: { url: testUrls[4] },
         keys: [ "url" ],
         expectedOutput: "https://test.com/?q=test&id=21afea8219bc#privacy"
     },
     {
-        input: { url: "-" },
+        input: { url: testUrls[5] },
+        keys: [ "url" ],
+        expectedOutput: "https://empty.value/?q=test&dm="
+    },
+    {
+        input: { url: testUrls[6] },
         keys: [ "url" ],
         expectedOutput: ""
     }
